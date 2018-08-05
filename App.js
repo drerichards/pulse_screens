1481f78533429087b836162779dfe6babb42ae76
Andre Richardson <g.andrerichards@gmail.com>
2018-08-04 22:01:08 -0400

    login screen done

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Login from './src/components/Login';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./src/components/Login";
 
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Login/>
+        <Login />
       </View>
     );
   }
@@ -17,18 +15,8 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#333',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#333"
+  }
 });
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,145 @@
-import React from 'react';
-import { View} from 'react-native';
-import { Button, Text } from 'native-base';
+import React from "react";
+import { View } from "react-native";
+import { Item, Input, Icon, Button, Text } from "native-base";
 
-const Login = ({album}) => {
-    return (
-        <View>
-            <Button>
-                <Text>hello</Text>
+const Login = () => {
+  return (
+    <View style={styles.mainContainerStyle}>
+      <View style={styles.subContainerStyle}>
+        <Icon style={[styles.logoStyle, styles.textStyle]} name="appstore" />
+        <Text style={[styles.logoText, styles.textStyle]}>Pulse</Text>
+        <View style={styles.inputSectionStyle}>
+          <Item>
+            <Input style={styles.textStyle} placeholder="email" />
+            <Icon style={styles.textStyle} active name="close" />
+          </Item>
+          <Item>
+            <Input style={styles.textStyle} placeholder="password" />
+            <Icon style={styles.textStyle} active name="close" />
+          </Item>
+          <View style={styles.forgotPswdBtn}>
+            <Button transparent>
+              <Text style={styles.buttonText}>Forgot Password?</Text>
             </Button>
+          </View>
         </View>
-    );
+
+        <View>
+          <Button
+            style={[styles.loginButtonStyle, styles.googleBtn]}
+            iconLeft
+            full
+            rounded
+          >
+            <Icon name="logo-googleplus" />
+            <Text style={styles.buttonText}>Sign in with Google</Text>
+          </Button>
+          <Button
+            style={[styles.loginButtonStyle, styles.facebookBtn]}
+            iconLeft
+            full
+            rounded
+          >
+            <Icon name="logo-facebook" />
+            <Text style={styles.buttonText}>Sign in with Facebook</Text>
+          </Button>
+          <Button
+            style={[styles.loginButtonStyle, styles.createAccBtn]}
+            iconLeft
+            full
+            rounded
+          >
+            <Text style={styles.createAccText}>Create an Account</Text>
+          </Button>
+        </View>
+      </View>
+      <View style={styles.signInStyle}>
+        <Text style={[styles.signInText, styles.textStyle]}>Sign In</Text>
+        <Button style={styles.signInBtn} iconRight rounded>
+          <Icon style={styles.signInIcon} name="arrow-forward" />
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = {
+  mainContainerStyle: {
+    flex: 1,
+    justifyContent: "space-around",
+    margin: 10
+  },
+  subContainerStyle: {
+    // flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  logoStyle: {
+    fontSize: 80
+  },
+  logoText: {
+    fontSize: 25
+  },
+  textStyle: {
+    color: "#fff"
+  },
+  inputSectionStyle: {
+    width: 250,
+    margin: 10,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  forgotPswdBtn: {
+    textAlign: "center"
+  },
+  buttonText: {
+    fontSize: 12,
+    color: "#fff",
+    marginHorizontal: "auto"
+  },
+  createAccText: {
+    fontSize: 12,
+    color: "#000",
+    marginHorizontal: "auto"
+  },
+  loginButtonStyle: {
+    width: 250,
+    margin: 5,
+    marginHorizontal: "auto"
+  },
+  googleBtn: {
+    backgroundColor: "#dd4b39"
+  },
+  facebookBtn: {
+    backgroundColor: "#3b5998"
+  },
+  createAccBtn: {
+    backgroundColor: "#fff"
+  },
+  signInStyle: {
+    flexDirection: "row",
+    alignSelf: "flex-end"
+  },
+  signInText: {
+    fontSize: 10,
+    margin: 20,
+    marginHorizontal: 10
+  },
+  signInBtn: {
+    width: 45,
+    textAlign: "center",
+    backgroundColor: "#ff0053",
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 3
+  },
+  signInIcon: {
+    textAlign: "center",
+    fontSize: 25,
+    paddingHorizontal: 3
+  }
 };
 
 export default Login;
